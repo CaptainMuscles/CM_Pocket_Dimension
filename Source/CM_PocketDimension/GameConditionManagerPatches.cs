fb9529c409a2b870d8efd114e215053b9324d49f
NathanHarbour <nathanharbour@gmail.com>
2020-12-22 00:32:39 -0600

    Solar flare inheritance
    The electricity disabled status inside a pocket dimension map will be inherited from the containing map.

---
+++ Source/CM_PocketDimension/GameConditionManagerPatches.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace CM_PocketDimension
+{
+    [StaticConstructorOnStartup]
+    public static class GameConditionManagerPatches
+    {
+        [HarmonyPatch(typeof(GameConditionManager))]
+        [HarmonyPatch("ElectricityDisabled", MethodType.Getter)]
+        public static class PocketDimensionBiomeGetter
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GameConditionManager __instance, ref bool __result)
+            {
+                if (__result && __instance.ownerMap != null)
+                {
+                    MapParent_PocketDimension mapParent = __instance.ownerMap.info.parent as MapParent_PocketDimension;
+                    if (mapParent != null)
+                    {
+                        Map containingMap = PocketDimensionUtility.GetHighestContainingMap(__instance.ownerMap);
+                        if (containingMap != __instance.ownerMap)
+                        {
+                            __result = containingMap.gameConditionManager.ElectricityDisabled;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
