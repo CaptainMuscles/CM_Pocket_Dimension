1afc086661c1fc899a518ce5ea1648bfa346ac8f
Sn1p3rr3c0n <Sn1p3rr3c0n@gmail.com>
2022-03-19 18:24:24 +0100

    Significantly improved the performance of the AnyPawnBlockingMapRemoval Patch

---
+++ Source/CM_PocketDimension/Comps/MapComponent_PocketDimension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace CM_PocketDimension.Comps
+{
+    class MapComponent_PocketDimension : MapComponent
+    {
+        public List<Building_PocketDimensionBox> Building_PocketDimensionBoxes { get; set; } = new List<Building_PocketDimensionBox>();
+
+
+             
+        public MapComponent_PocketDimension(Map map) : base(map)
+        {
+
+        }
+    }
+}
